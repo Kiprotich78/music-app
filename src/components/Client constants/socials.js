b208849d0e9cf7992f537c4bc5ad868dd3597686
JohnMigwi <johnmigwi101@gmail.com>
2023-05-31 17:23:59 +0300

    Social links for the footers used

---
+++ src/components/Client constants/socials.js
@@ -0,0 +1,20 @@
+import { FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
+import React from 'react';
+
+export const Socials = [
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/Johnmiskyjm',
+    icon: <FaTwitter />,
+  },
+  {
+    name: 'Linked-In',
+    url: 'https://www.linkedin.com/in/john-migwi-62996621b/',
+    icon: <FaLinkedin />,
+  },
+  {
+    name: 'Github',
+    url: 'https://github.com/JohnMigwi',
+    icon: <FaGithub />,
+  },
+];
