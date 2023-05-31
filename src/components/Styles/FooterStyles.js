6ff068970c2dc6a9d8f52c48a2ccdaf48bb42755
JohnMigwi <johnmigwi101@gmail.com>
2023-05-31 21:21:38 +0300

    changes on footer styles

--- src/components/Styles/FooterStyles.js
+++ src/components/Styles/FooterStyles.js
@@ -3,29 +3,35 @@ import styled from 'styled-components';
 export const FooterStyles = styled.footer`
   padding: 1rem 0 1rem 0;
   text-align: center;
-  background-color:  #FF66CC;
+  background-color: #000000;
   
   .footer__text {
     margin: 0 auto;
     margin-top: 1rem;
     max-width: 900px;
+    color: white;
+    font-family: Times New Roman;
+    font-size: 10;
+
   }
 
   .footer__menu {
     margin-top: 1rem;
+    background-color: white;
 
     li {
       display: inline-block;
       margin: 0 1rem;
 
       a {
-        color: var(--white-1);
+        color: white;
         font-size: 1.6rem;
       }
     }
   }
   .footer__social {
     margin-top: 1.5rem;
+    color: white;
     li {
       display: inline-block;
       margin: 0 0.5rem;
@@ -43,7 +49,9 @@ export const FooterStyles = styled.footer`
   }
   .copy {
     margin-top: 1rem;
-    color: var(--gray);
+    color: white;
     font-size: 1.2rem;
+    font-family: Times New Roman;
+    font-size: 10;
   }
 `;
