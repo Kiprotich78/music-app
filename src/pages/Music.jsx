47b466ec6467b2155560d180e838f1579733942e
Kiprotich Shadrack <shadrackr057@gmail.com>
2023-06-03 10:41:30 +0300

    merged John-music branch, fixed overflowing content towards x axis

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -23,15 +23,17 @@ const Music = () => {
   }
 
   return (
-    <><section className='w-screen h-screen bg-gradient-to-b from-pink-300 to-pink-700 flex'>
-      <div className={`absolute ${showSideBar ? "translate-x-0" : "-translate-x-full"} w-full md:translate-x-0 md:static md:w-64 h-full transition-all z-20`}>
-        <SideBar removeSidebar={removeSidebar} handleActivePage={handleActivePage} activeSection={activeSection} />
-      </div>
-      <div className='main-width h-full'>
-        <Main addSidebar={addSidebar}/>
-      </div>
+    <>
+      <section className='w-screen h-screen bg-gradient-to-b from-pink-300 to-pink-700 flex overflow-x-hidden'>
+        <div className={`absolute ${showSideBar ? "translate-x-0" : "-translate-x-full"} w-full md:translate-x-0 md:static md:w-64 h-full transition-all z-20`}>
+          <SideBar removeSidebar={removeSidebar} handleActivePage={handleActivePage} activeSection={activeSection} />
+        </div>
+        <div className='main-width h-full'>
+          <Main addSidebar={addSidebar}/>
+        </div>
 
-    </section><Footer /></>
+      </section><Footer />
+    </>
   )
 }
 
