cd876e42255e77f779737fbb2133aae8027cda5f
Kiprotich Shadrack <shadrackr057@gmail.com>
2023-06-05 10:06:20 +0300

    set height of the main content to 100dvh which is dynamic height

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -24,7 +24,7 @@ const Music = () => {
 
   return (
     <>
-      <section className='w-screen h-screen bg-gradient-to-b from-pink-300 to-pink-700 flex overflow-x-hidden'>
+      <section className='w-screen mainHeight bg-gradient-to-b from-pink-300 to-pink-700 flex overflow-x-hidden'>
         <div className={`absolute ${showSideBar ? "translate-x-0" : "-translate-x-full"} w-full md:translate-x-0 md:static md:w-64 h-full transition-all z-20`}>
           <SideBar removeSidebar={removeSidebar} handleActivePage={handleActivePage} activeSection={activeSection} />
         </div>
